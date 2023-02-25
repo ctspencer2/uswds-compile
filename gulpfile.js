f18f21910ec507f321ea106712f9a91e2033c28b
ctspencer2 <118775383+ctspencer2@users.noreply.github.com>
2023-02-25 08:18:56 -0800

    compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
+const uswds = require("@uswds/compile");
+uswds.settings.version = 3;
+exports.compile = uswds.compile;
+exports.watch = uswds.watch;
+exports.init = uswds.init;
+exports.update = uswds.updateUswds;
+exports.default = uswds.watch;
 const autoprefixer = require("autoprefixer");
 const csso = require("postcss-csso");
 const { src, dest, series, parallel, watch } = require("gulp");
